0e0c046e6062e3d81ce342834e44c1202a4e5995
Samarth Ghante <samarthghante.dev@gmail.com>
2024-11-13 12:43:48 +0530

    [refactor] added skeleton to some components while others need changes in nav-items

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -6,6 +6,7 @@ import { useLocalStorage } from "usehooks-ts";
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { Accordion } from "@/components/ui/accordion";
 
 import { NavItem, Organization } from "./nav-item";
@@ -48,6 +49,22 @@ export default function Sidebar({
     }));
   };
 
+  if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+    return (
+      <>
+        <div className="flex items-center justify-between mb-2">
+          <Skeleton className="h-10 w-[50%]" />
+          <Skeleton className="h-10 w-10" />
+        </div>
+        <div className="space-y-2">
+          {/* <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton /> */}
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <div className="font-medium text-medium flex items-center mb-1">
@@ -69,16 +86,15 @@ export default function Sidebar({
         defaultValue={defaultAccordionValue}
         className="space-y-2"
       >
-        {userMemberships.data &&
-          userMemberships.data.map(({ organization }) => (
-            <NavItem
-              key={organization.id}
-              isActive={activeOrganization?.id === organization.id}
-              isExpanded={expanded[organization.id]}
-              organization={organization as Organization}
-              onExpand={onExpand}
-            />
-          ))}
+        {userMemberships.data.map(({ organization }) => (
+          <NavItem
+            key={organization.id}
+            isActive={activeOrganization?.id === organization.id}
+            isExpanded={expanded[organization.id]}
+            organization={organization as Organization}
+            onExpand={onExpand}
+          />
+        ))}
       </Accordion>
     </>
   );
