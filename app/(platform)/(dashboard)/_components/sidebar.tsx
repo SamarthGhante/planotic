c001096ded06ad3da060fc2ca7803afbc1244dbe
Samarth Ghante <samarthghante.dev@gmail.com>
2024-11-14 09:58:45 +0530

    [feat] sidebar

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -57,9 +57,9 @@ export default function Sidebar({
           <Skeleton className="h-10 w-10" />
         </div>
         <div className="space-y-2">
-          {/* <NavItem.Skeleton />
           <NavItem.Skeleton />
-          <NavItem.Skeleton /> */}
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
         </div>
       </>
     );
