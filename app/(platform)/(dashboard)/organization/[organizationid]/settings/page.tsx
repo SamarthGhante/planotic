5fd195e1e404030672d4b9e69e7ba6651bd0525e
Samarth Ghante <samarthghante.dev@gmail.com>
2024-11-15 12:21:18 +0530

    [feat] organization/settings

--- app/(platform)/(dashboard)/organization/[organizationid]/settings/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationid]/settings/page.tsx
@@ -1,3 +1,29 @@
+import { ClerkLoading, OrganizationProfile } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+
 export default function SettingsPage() {
-  return <div>Settings Page</div>;
+  return (
+    <div className="w-full">
+      <ClerkLoading>
+        <div className="h-full flex items-center justify-center text-black">
+          <Loader2 className="h-8 w-8 text-purple-500 animate-spin" />
+        </div>
+      </ClerkLoading>
+      <OrganizationProfile
+        appearance={{
+          elements: {
+            rootBox: {
+              boxShadow: "none",
+              width: "100%",
+            },
+            card: {
+              border: "1px solid #e5e5e5",
+              boxShadow: "none",
+              width: "100%",
+            },
+          },
+        }}
+      />
+    </div>
+  );
 }
