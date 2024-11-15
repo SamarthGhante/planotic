e72a194e2b295212d0e1c0c70486c1e651f9a211
Samarth Ghante <samarthghante.dev@gmail.com>
2024-11-16 03:29:50 +0530

    [chore] board-info section layout setup

--- app/(platform)/(dashboard)/organization/[organizationid]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationid]/page.tsx
@@ -1,3 +1,15 @@
+import Info from "./_components/info";
+import BoardList from "./_components/board-list";
+import { Separator } from "@/components/ui/separator";
+
 export default function OrganizationIdPage() {
-  return <div>Oraganization Page</div>;
+  return (
+    <div className="w-full">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
+    </div>
+  );
 }
