9372461b7b0872331ef7b90577e58e196c9b92b2
Samarth Ghante <samarthghante.dev@gmail.com>
2024-11-17 04:58:26 +0530

    [add] create-board button inside board-list

--- app/(platform)/(dashboard)/organization/[organizationid]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationid]/_components/board-list.tsx
@@ -1,3 +1,34 @@
-export default function BoardList() {
-  return <div>Board List</div>;
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { User2 } from "lucide-react";
+
+import checkSubscription from "@/lib/subscription";
+
+export default async function BoardList() {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return redirect("/select-org");
+  }
+
+  const isPro = checkSubscription();
+
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center font-semibold text-lg text-neutral-700">
+        <User2 className="h-6 w-6 mr-2" />
+        Your boards
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div
+          role="button"
+          className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
+        >
+          <p className="text-sm">Create new board</p>
+          <span className="text-xs">{isPro ? "Unlimited" : `5 remaining`}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
