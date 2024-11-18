6d5b6c706929b3015eef75aceaa988c5672b63b0
Samarth Ghante <samarthghante.dev@gmail.com>
2024-11-18 12:18:40 +0530

    [chore] create-board form setup with popover form.

--- app/(platform)/(dashboard)/organization/[organizationid]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationid]/_components/board-list.tsx
@@ -5,6 +5,7 @@ import { HelpCircle, User2 } from "lucide-react";
 
 import checkSubscription from "@/lib/subscription";
 import { Hint } from "@/components/hint";
+import FormPopover from "@/components/form/form-popover";
 
 export default async function BoardList() {
   const { orgId } = auth();
@@ -22,21 +23,25 @@ export default async function BoardList() {
         Your boards
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-        <div
-          role="button"
-          className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
-        >
-          <p className="text-sm">Create new board</p>
-          <span className="text-xs">{isPro ? "Unlimited" : `5 remaining`}</span>
-          <Hint
-            sideOffset={45}
-            description={`
-                Free Workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace.
-              `}
+        <FormPopover side="right" sideOffset={10}>
+          <div
+            role="button"
+            className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
           >
-            <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
-          </Hint>
-        </div>
+            <p className="text-sm">Create new board</p>
+            <span className="text-xs">
+              {isPro ? "Unlimited" : `5 remaining`}
+            </span>
+            <Hint
+              sideOffset={45}
+              description={`
+                Free accounts are limited to 5 open boards. To create unlimited boards, please upgrade your workspace.
+              `}
+            >
+              <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
+            </Hint>
+          </div>
+        </FormPopover>
       </div>
     </div>
   );
---
+++ components/form/form-popover.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+interface FormPopoverProps {
+  children: React.ReactNode;
+  side?: "left" | "right" | "top" | "bottom";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+}
+
+export default function FormPopover({
+  children,
+  side = "bottom",
+  align,
+  sideOffset = 0,
+}: FormPopoverProps) {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>{children}</PopoverTrigger>
+      <PopoverContent
+        align={align}
+        className="w-80 pt-3"
+        side={side}
+        sideOffset={sideOffset}
+      >
+        Create Board Form
+      </PopoverContent>
+    </Popover>
+  );
+}
