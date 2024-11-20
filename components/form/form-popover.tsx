53f5a65878cf6853a20bb0755e918c1411b40a77
Samarth Ghante <samarthghante.dev@gmail.com>
2024-11-20 16:19:00 +0530

    [feat] create-board (form-popover) component

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -6,6 +6,9 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 
+import { FormInput } from "./form-input";
+import { FormSubmit } from "./form-submit";
+
 interface FormPopoverProps {
   children: React.ReactNode;
   side?: "left" | "right" | "top" | "bottom";
@@ -19,6 +22,12 @@ export const FormPopover = ({
   align,
   sideOffset = 0,
 }: FormPopoverProps) => {
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    console.log({
+      boardTitle: title,
+    });
+  };
   return (
     <Popover>
       <PopoverTrigger asChild>{children}</PopoverTrigger>
@@ -28,8 +37,16 @@ export const FormPopover = ({
         side={side}
         sideOffset={sideOffset}
       >
-        Create Board Form
+        <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+          Create board
+        </div>
+        <form action={onSubmit} className="space-y-4">
+          <div className="space-y-4">
+            <FormInput id="title" label="Board title" type="text" />
+          </div>
+          <FormSubmit className="w-full">Create</FormSubmit>
+        </form>
       </PopoverContent>
     </Popover>
   );
-}
+};
