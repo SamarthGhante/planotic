ec8f49b6534c239eee8b79dcfba6b0348658202a
Samarth Ghante <samarthghante.dev@gmail.com>
2024-11-25 13:55:33 +0530

    [added] schema for create-board action

--- actions/create-board/schema.ts
+++ actions/create-board/schema.ts
@@ -1,1 +1,16 @@
-/* define schema of data for create-board action */
+import { z } from "zod";
+
+export const CreateBoard = z.object({
+  title: z
+    .string({
+      required_error: "Title is required",
+      invalid_type_error: "Title is required",
+    })
+    .min(3, {
+      message: "Title is too short.",
+    }),
+  image: z.string({
+    required_error: "Image is required",
+    invalid_type_error: "Image is required",
+  }),
+});
