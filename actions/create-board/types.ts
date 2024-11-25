51e734006ed441a921e4cd505893487a12c51a51
Samarth Ghante <samarthghante.dev@gmail.com>
2024-11-25 14:05:53 +0530

    [added] types for create-board action

--- actions/create-board/types.ts
+++ actions/create-board/types.ts
@@ -1,1 +1,9 @@
-/* define types for create-board action */
+import { z } from "zod";
+import { Board } from "@prisma/client";
+
+import { ActionState } from "@/lib/create-safe-action";
+
+import { CreateBoard } from "./schema";
+
+export type InputType = z.infer<typeof CreateBoard>;
+export type ReturnType = ActionState<InputType, Board>;
