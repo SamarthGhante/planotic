e789b417d3a0e6022c59d78ca53ed6487b854e95
Samarth Ghante <samarthghante.dev@gmail.com>
2024-11-28 11:20:38 +0530

    [chore] board-title-form component to show and update board title

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,4 +1,5 @@
 import { Board } from "@prisma/client";
+import { BoardTitleForm } from "./board-title-form";
 
 interface boardNavbarProps {
   data: Board;
@@ -7,7 +8,7 @@ interface boardNavbarProps {
 export default async function BoardNavbar({ data }: boardNavbarProps) {
   return (
     <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
-      {data.title}
+      <BoardTitleForm data={data} />
     </div>
   );
 }
