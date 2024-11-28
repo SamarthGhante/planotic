d9c1086b7bec29aeca037f01fb13d5b231d3ae3a
Samarth Ghante <samarthghante.dev@gmail.com>
2024-11-28 09:20:01 +0530

    [chore] boardId page and layout setup

---
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,41 @@
+import { auth } from "@clerk/nextjs";
+import { notFound, redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+
+interface boardIdProps {
+  children: React.ReactNode;
+  params: {
+    boardId: string;
+  };
+}
+
+export default async function BoardIdLayout({
+  children,
+  params,
+}: boardIdProps) {
+  const { orgId } = auth();
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  if (!board) {
+    notFound();
+  }
+  return (
+    <div
+      className="relative h-full bg-no-repeat bg-cover bg-center"
+      style={{ backgroundImage: `url(${board.imageFullUrl})` }}
+    >
+      <div className="absolute inset-0 bg-black/10" />
+      <main className="relative pt-28 h-full">{children}</main>
+    </div>
+  );
+}
---
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -0,0 +1,5 @@
+export default function BoardIdPage() {
+  return (
+    <div className="h-full items-center justify-center">Board Content</div>
+  );
+}
