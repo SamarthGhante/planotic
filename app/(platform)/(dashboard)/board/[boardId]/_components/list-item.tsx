2bd35954d9d18dc7704b695fdf3330c54e533599
Samarth Ghante <samarthghante.dev@gmail.com>
2024-12-02 14:00:28 +0530

    [update] ui/ux card-form, form-text-area

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -1,15 +1,39 @@
 import { List } from "@prisma/client";
+
+import { CardForm } from "./card-form";
 import { ListHeader } from "./list-header";
+import { ElementRef, useRef, useState } from "react";
 
 interface listItemProps {
   data: List;
 }
 
 export const ListItem = ({ data }: listItemProps) => {
+  const textareaRef = useRef<ElementRef<"textarea">>(null);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      textareaRef.current?.focus();
+    });
+  };
   return (
     <li className="shrink-0 h-full w-[272px] select-none">
       <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-1">
         <ListHeader data={data} />
+        <CardForm
+          listId={data.id}
+          ref={textareaRef}
+          isEditing={isEditing}
+          enableEditing={enableEditing}
+          disableEditing={disableEditing}
+        />
       </div>
     </li>
   );
