c2d5fcb9d79429975f7b64cb84a2ce4778c3e6b5
Samarth Ghante <samarthghante.dev@gmail.com>
2024-12-04 04:11:02 +0530

    [add] query-provider, tanstack-query

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from "sonner";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 interface childrenProp {
   children: React.ReactNode;
@@ -9,9 +10,11 @@ interface childrenProp {
 export default function PlatformLayout({ children }: childrenProp) {
   return (
     <ClerkProvider>
-      <Toaster />
-      <ModalProvider />
-      {children}
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   );
 }
